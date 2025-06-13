c8d89663ccf40c584e595092ab20f7b69a1c2e89
Jhanvi Yadav <jhanviyadav586@gmail.com>
2025-06-13 21:30:34 +0530

    Add Login Frame

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -3,33 +3,34 @@ package university.management.system;
 import javax.swing.*;
 import java.awt.*;
 
-public class Splash extends JFrame {
+public class Splash extends JFrame implements Runnable { // Implements Runnable interface
     Thread t;
 
-    Splash(){
+    Splash() {
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/first.png"));
-        Image i2 = i1.getImage().getScaledInstance(1000,700,Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage().getScaledInstance(900, 600, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel img = new JLabel(i3);
         add(img);
 
-        t = new Thread(String.valueOf(this));
-        t.start();
+        setSize(900, 600); // Explicitly set window size
+        setLocationRelativeTo(null); // Center the window
         setVisible(true);
 
-        setVisible(true);
+        t = new Thread(this); // Correct way to create a thread
+        t.start();
     }
-    public void run(){
+
+    public void run() {
         try {
             Thread.sleep(7000);
             setVisible(false);
-            //next class
-
-        }catch (Exception e){
+            new Login(); // Assuming Login is another JFrame class
+        } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
+
     public static void main(String[] args) {
         new Splash();
     }
