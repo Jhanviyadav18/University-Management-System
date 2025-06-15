dc2e5b103d04246f61db23688ea97db98abe343f
Jhanvi Yadav <jhanviyadav586@gmail.com>
2025-06-15 12:42:25 +0530

    Build Connection

--- src/university/management/system/Login.java
+++ src/university/management/system/Login.java
@@ -1,81 +1,104 @@
 package university.management.system;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+        import java.awt.*;
+        import java.awt.event.*;
+        import java.sql.*;
 
 public class Login extends JFrame implements ActionListener {
     JTextField textFieldName;
     JPasswordField passwordField;
     JButton login, back;
 
-    Login(){
+    Login() {
+        setTitle("Login");
 
         JLabel labelName = new JLabel("Username");
-        labelName.setBounds(40,20,100,20);
+        labelName.setBounds(40, 20, 100, 20);
         add(labelName);
 
         textFieldName = new JTextField();
-        textFieldName.setBounds(150,20,150,20);
+        textFieldName.setBounds(150, 20, 150, 20);
         add(textFieldName);
 
-        JLabel labelpass = new JLabel("Password");
-        labelpass.setBounds(40,70,100,20);
-        add(labelpass);
+        JLabel labelPass = new JLabel("Password");
+        labelPass.setBounds(40, 70, 100, 20);
+        add(labelPass);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(150,70,150,20);
+        passwordField.setBounds(150, 70, 150, 20);
         add(passwordField);
 
-
         login = new JButton("Login");
-        login.setBounds(40,140,120,30);
+        login.setBounds(40, 140, 120, 30);
         login.setBackground(Color.BLACK);
         login.setForeground(Color.WHITE);
         login.addActionListener(this);
         add(login);
 
         back = new JButton("Back");
-        back.setBounds(180,140,120,30);
+        back.setBounds(180, 140, 120, 30);
         back.setBackground(Color.BLACK);
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/second.png"));
-        Image i2 = i1.getImage().getScaledInstance(200,200,Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel img = new JLabel(i3);
-        img.setBounds(350,20,200,200);
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        JLabel img = new JLabel(new ImageIcon(i2));
+        img.setBounds(350, 20, 200, 200);
         add(img);
 
         ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icon/loginback.png"));
-        Image i22 = i11.getImage().getScaledInstance(600,300,Image.SCALE_DEFAULT);
-        ImageIcon i33 = new ImageIcon(i22);
-        JLabel image = new JLabel(i33);
-        image.setBounds(0,0,600,300);
+        Image i22 = i11.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
+        JLabel image = new JLabel(new ImageIcon(i22));
+        image.setBounds(0, 0, 600, 300);
         add(image);
 
-
-        setSize(600,300);
-        setLocation(450,250);
+        setSize(600, 300);
+        setLocation(450, 250);
         setLayout(null);
         setVisible(true);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == login){
+        if (e.getSource() == login) {
+            String username = textFieldName.getText();
+            String password = new String(passwordField.getPassword()); // secure way to get password
 
-        }else {
+            try {
+                Conn c = new Conn();
+
+                // Use PreparedStatement to prevent SQL injection
+                String query = "SELECT * FROM login WHERE username = ? AND password = ?";
+                PreparedStatement pst = c.connection.prepareStatement(query);
+                pst.setString(1, username);
+                pst.setString(2, password);
+
+                ResultSet rs = pst.executeQuery();
+
+                if (rs.next()) {
+                    setVisible(false);
+                    // TODO: Open next class here
+                    JOptionPane.showMessageDialog(null, "Login Successful!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid username or password");
+                }
+
+                rs.close();
+                pst.close();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+        } else if (e.getSource() == back) {
             setVisible(false);
+            // Optional: navigate to the previous screen
         }
-
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Login();
     }
 }
